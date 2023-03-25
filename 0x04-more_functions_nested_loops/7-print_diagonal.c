fbe6c4366c08ae3d759a962dd1cd53916d9bdcbd
pedro-bgb <badreamellal@gmail.com>
2023-03-24 17:19:08 -0700

    7 diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,17 +1,31 @@
-On branch master
-Your branch is up to date with 'origin/master'.
+#include "main.h"
 
-Changes not staged for commit:
-  (use "git add <file>..." to update what will be committed)
-  (use "git restore <file>..." to discard changes in working directory)
-	modified:   ../0x03-debugging/2-largest_number.c
-	modified:   ../0x03-debugging/2-main.c
-	modified:   7-print_diagonal.c
+/**
+ * print_diagonal - entry point
+ *	@n: int number
+ * Description: --
+ * Return: --
+ */
+void print_diagonal(int n)
+{
+	int i = 0;
+	int j;
 
-Untracked files:
-  (use "git add <file>..." to include in what will be committed)
-	../0x03-debugging/2-main
-	../101-print_comb4.c
-	../RADME.md
-
-no changes added to commit (use "git add" and/or "git commit -a")
+	while (i < n)
+	{
+		if (n > 0)
+		{
+			j = 0;
+			while (j <= i - 1)
+			{
+				_putchar(' ');
+				j++;
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+		i++;
+	}
+	if (n <= 0)
+		_putchar('\n');
+}
