2e672df40938b4bd79f15b6f554545befaf4035e
pedro-bgb <badreamellal@gmail.com>
2023-03-27 14:26:19 -0700

    This report by its very length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,20 +1,15 @@
 #include "main.h"
+#include <string.h>
 
 /**
- *  * _strlen - returns the length of a string
- *   * @s: string
+ *  * _strlen - prints lenght of a string
+ *   * @s: variable to be checked
  *    *
- *     * Return: the length of the string
+ *     * Descriptipn: prints length of strin
+ *     *
+ *Return: Always 0
  */
 int _strlen(char *s)
 {
-		int i;
-
-			i = 0;
-
-	while (*s != '\0')
-
-	i++;
-
-		return (i);
+		return (strlen(s));
 }
