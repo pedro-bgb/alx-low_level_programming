6a9519011c94c46d2d32c93e5f0f3e8d6f65f967
pedro-bgb <badreamellal@gmail.com>
2023-03-27 14:16:03 -0700

    This report by its very length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -5,17 +5,16 @@
  *   * @s: string
  *    *
  *     * Return: the length of the string
- *      */
+ */
 int _strlen(char *s)
 {
 		int i;
 
 			i = 0;
 
-				while (s[i] != '\0')
-						{
-									i++;
-										}
+	while (*s != '\0')
 
-					return (i);
+	i++;
+
+		return (i);
 }
