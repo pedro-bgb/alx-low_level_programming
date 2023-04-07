5e873aa4084929c45117582ef05eb22f80b90f9e
pedro-bgb <badreamellal@gmail.com>
2023-04-06 18:16:12 -0700

    Inception. Now, before you bother telling me it's impossible

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * wildcmp - check if strings could be considered identical
+ * @s1: string
+ * @s2: string
+ *
+ * Return: 1 if are considered identical.
+ */
+int wildcmp(char *s1, char *s2)
+{
+	return (str_checker(s1, s2, 0, 0));
+}
+/**
+ * str_checker - check if two strings are identical
+ * @s1: string_1 base address
+ * @s2: string_2 base address
+ * @i: left index.
+ * @j: special index
+ * Return: 1 if s is palindrome, 0 otherwise
+ */
+int str_checker(char *s1, char *s2, int i, int j)
+{
+	if (s1[i] == '\0' && s2[j] == '\0')
+		return (1);
+	if (s1[i] == s2[j])
+		return (str_checker(s1, s2, i + 1, j + 1));
+	if (s1[i] == '\0' && s2[j] == '*')
+		return (str_checker(s1, s2, i, j + 1));
+	if (s2[j] == '*')
+		return (str_checker(s1, s2, i + 1, j) || str_checker(s1, s2, i, j + 1));
+	return (0);
+}
